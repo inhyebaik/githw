ba75209aa7c94cfee58bde168e59ebe15e1f7e1b
Inhye <inb125@mail.harvard.edu>
2017-08-02 21:59:51 -0700

    changes to be committed

---
+++ gitfile.rb
@@ -0,0 +1,20 @@
+puts "hello!"
+
+puts "Now I'm added and saved"
+
+puts "I was just reset!"
+end
+
+
+whats going on?
+
+Untracked files:
+  (use "git add <file>..."  include in what will be committed)
+
+	gitfile.rb
+
+ hello?
+  dsfas
+  f
+
+fasdfsd
